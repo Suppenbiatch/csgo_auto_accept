7dae7011addca256173916b997c2807a81748948
Suppe <zwergi315@gmail.com>
2023-06-19 18:02:02 +0200

    Update WebHookCreator.py
    fixed

--- objects/WebHookCreator.py
+++ objects/WebHookCreator.py
@@ -7,15 +7,14 @@ from argparse import ArgumentParser
 def command_converter(commands: list[str]):
     config = ConfigParser()
     config.read('../config.ini')
-    ip_addr = config.get('csgostats.gg', 'webserver ip')
-    port = config.getint('csgostats.gg', 'webserver port')
-
-    return f'http:{ip_addr}:{port}/console?input={urllib.parse.quote(json.dumps(commands))}'
+    ip_addr = config.get('HotKeys', 'webhook ip')
+    port = config.get('HotKeys', 'webhook port')
+    return f'http://{ip_addr}:{port}/console?input={urllib.parse.quote(json.dumps(commands))}'
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('commands', nargs='+', type=str)
-    args = parser.parse_args('say hello, say goodbye')
+    args = parser.parse_args()
     commands = ''.join(args.commands).split(', ')
     print(command_converter(commands))
 
